458a6df9a6a4f32631a3fa8a0eb0b46f71aac01c
Yousef Farge <yousef.farge@gmail.com>
2024-10-31 14:40:42 -0500

    feat: added backdrop blur Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
-export const Header = () => {
-  const items = [
-    { title: "home", href: "/" },
-    { title: "projects", href: "/projects" },
-    { title: "blog", href: "/blog" },
-  ];
+const links = [
+  { title: "home", href: "/" },
+  { title: "projects", href: "/projects" },
+  { title: "blog", href: "/blog" },
+];
 
+export const Header = () => {
   return (
-    <header className="py-6">
-      <nav className="my-4">
-        <ul className="flex gap-8 text-lg">
-          {items.map(({ title, href }) => {
+    <header className="sticky top-0 z-50 bg-background/75 py-6 backdrop-blur-sm">
+      <nav>
+        <ul className="flex gap-4 sm:gap-8">
+          {links.map(({ title, href }) => {
             return (
               <li key={title}>
                 <Link href={href}>{title}</Link>
